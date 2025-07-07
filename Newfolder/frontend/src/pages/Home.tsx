9fefd22b948b91e790dfa56c38b6356f8805c0a3
Altibbe-Hedamo <has.login9@gmail.com>
2025-07-07 17:59:31 +0530

    Remove unused TestimonialCarousel import to fix TS6133 error

--- Newfolder/frontend/src/pages/Home.tsx
+++ Newfolder/frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import heroBackground from '/homepage.webp';
-import { TestimonialCarousel } from '../components/testimonial';
 import Footer from '../components/Footer';
 import React from 'react'; // Added for React.useState
 
